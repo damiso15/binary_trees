16a6c54e747a3ebee0fbe3f4d6ebd93e9e649ac9
damiso15 <damiso15@yahoo.com>
2023-05-30 18:55:39 +0100

    9. Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,16 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	if (left_height >  right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+	return (left_height > right_height ? left_height : right_height);
 }
