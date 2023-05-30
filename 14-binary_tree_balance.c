d0928079be5760e439490ed5f39ca3614035bd9c
damiso15 <damiso15@yahoo.com>
2023-05-30 21:06:51 +0100

    14. Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,14 +12,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->left);
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+	right_height = tree->right ? 1 +binary_tree_height(tree->right) : 1;
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	return (left_height > right_height ? left_height : right_height);
 }
 
-/*
- * binary_tree_balance: A function that measures the balance factor of a
+
+/**
+ * binary_tree_balance - A function that measures the balance factor of a
  *			binary tree
  * @tree: a pointer to the root node of the tree to measure the balance factor
  * Return: if tree is NULL return 0
@@ -31,7 +32,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (balance_factor);
 
-	balance_factor = binary_tree_height(tree->left) - binary_tree_height(tree->right);
+	balance_factor = binary_tree_height(tree->left) -
+		binary_tree_height(tree->right);
 
 	return (balance_factor);
 }
---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - A function that checks if a binary tree is full
+ * @tree: a pointer to the root node of the tree to check
+ * Return: if tree is NULL return 0
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+}
